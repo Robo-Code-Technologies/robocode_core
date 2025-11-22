1a3b059a07656be6ed58f2523d6c52a0415bae54
Yebione <kylebillones.63@gmail.com>
2025-11-22 20:03:30 +0800

    style: fixed vertical carousel

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::get('/component-builder', function () {
-    return Inertia::render('ComponentBuilder');
-})->name('component-builder');
-
 // Route::middleware(['auth', 'verified'])->group(function () {
 //     Route::get('dashboard', function () {
 //         return Inertia::render('dashboard');
